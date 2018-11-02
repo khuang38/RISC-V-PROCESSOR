c5f31b9788c15da1dc7e35eee50f4712db594e52
Caiting Wu <cwu72@dcl-l440-23.ews.illinois.edu>
2018-11-01 21:56:24 -0500

    Arbiter added, cache_group modified

--- mp3/cache_group.sv
+++ mp3/cache_group.sv
@@ -9,8 +9,8 @@ module cache_group
     input [3:0]    cmem_byte_enable_a,
     input [31:0]   cmem_address_a,
     input [31:0]   cmem_wdata_a,
-	 
-	 output         cmem_resp_b,
+
+	output         cmem_resp_b,
     output [31:0]  cmem_rdata_b,
     input          cmem_read_b,
     input          cmem_write_b,
@@ -26,8 +26,82 @@ module cache_group
     output [255:0] pmem_wdata
 );
 
+// Internal Signals Instantiation
+logic i_resp, i_write, i_read, i_wdata;
+logic [255:0] i_wdata;
+logic [31:0] i_addr;
+logic [255:0] i_rdata;
 
-// TODO::
+logic d_resp, d_write, d_read, d_wdata;
+logic [255:0] d_wdata;
+logic [31:0] d_addr;
+logic  [255:0] d_rdata;
 
+
+cache instruct_cache
+(
+    .clk,
+    .cmem_resp(cmem_resp_a),
+    .cmem_rdata(cmem_rdata_a),
+    .cmem_read(cmem_read_a),
+    .cmem_write(cmem_write_a),
+    .cmem_byte_enable(cmem_byte_enable_a),
+    .cmem_address(cmem_address_a),
+    .cmem_wdata(cmem_wdata_a),
+
+    .pmem_resp(i_resp),
+    .pmem_rdata(i_rdata),
+    .pmem_read(i_read),
+    .pmem_write(i_write),
+    .pmem_address(i_addr),
+    .pmem_wdata(i_wdata)
+);
+
+
+cache data_cache
+(
+    .clk,
+    .cmem_resp(cmem_resp_b),
+    .cmem_rdata(cmem_rdata_b),
+    .cmem_read(cmem_read_b),
+    .cmem_write(cmem_write_b),
+    .cmem_byte_enable(cmem_byte_enable_b),
+    .cmem_address(cmem_address_b),
+    .cmem_wdata(cmem_wdata_b),
+
+    .pmem_resp(d_resp),
+    .pmem_rdata(d_rdata),
+    .pmem_read(d_read),
+    .pmem_write(d_write),
+    .pmem_address(d_addr),
+    .pmem_wdata(d_wdata)
+);
+
+arbiter arbiter
+(
+    .clk,
+
+    .i_resp,
+    .i_write,
+    .i_read,
+    .i_wdata,
+    .i_addr,
+    .i_rdata,
+
+    // Signals for data cache
+    .d_resp,
+    .d_write,
+    .d_read,
+    .d_wdata,
+    .d_addr,
+    .d_rdata,
+
+    .pmem_resp,
+    .pmem_rdata,
+    .pmem_read,
+    .pmem_write,
+    .pmem_address,
+    .pmem_wdata
+);
 
 endmodule : cache
